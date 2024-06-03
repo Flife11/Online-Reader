1d0e6587eea88f9d360ba310fab2718d1844665d
sonny <sonny.l@itcgroup.io>
2024-06-03 22:27:14 +0700

    add missing search action

--- backend/Controllers/NovelController.js
+++ backend/Controllers/NovelController.js
@@ -39,4 +39,16 @@ module.exports = {
             res.status(500).json({ message: error.message });
         }
     },
+
+    SearchNovel: async function (req, res) {
+        try {
+            const Domain = new Factory(req.params.domain);
+            let Data = await Domain.SearchNovel(req.query.keyword,req.query.page);
+            res.status(200).json({ data: Data });
+        }
+        catch (error) {
+            //console.log(error)
+            res.status(500).json({ message: error });
+        }
+    },
 }
